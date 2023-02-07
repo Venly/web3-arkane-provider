7823416e305a62c4a924dc997436f38e40861b6a
David Fu <davidzwfu@gmail.com>
2023-02-07 13:59:58 +0400

    add webpack for single js file build

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ],
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: {
+    main: "./src/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: "web3-provider.js" // <--- Will be compiled to this single file
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    fallback: {
+      "stream": false,
+    }
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }
+    ]
+  }
+};
